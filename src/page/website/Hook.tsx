7e039ff2d92759fd03f10ebc91a46b81c62f8615
Duong Duc <duonganhduc6a4@gmail.com>
2024-08-02 16:22:12 +0700

    done

--- src/page/website/Hook.tsx
+++ src/page/website/Hook.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import useFetchProducts from './useFetchProducts';
+// import React from 'react';
+// import useFetchProducts from './useFetchProducts';
 
-const ProductList = () => {
-  const { products, isLoading, isError, error, refetch } = useFetchProducts();
+// const ProductList = () => {
+//   const { products, isLoading, isError, error, refetch } = useFetchProducts();
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
 
-  if (isError) {
-    return <div>Error: {error.message}</div>;
-  }
+//   if (isError) {
+//     return <div>Error: {error.message}</div>;
+//   }
 
-  return (
-    <div>
-      <h1>Product List</h1>
-      <button onClick={refetch}>Refetch Products</button>
-      <ul>
-        {products.map(product => (
-          <li key={product.id}>{product.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Product List</h1>
+//       <button onClick={refetch}>Refetch Products</button>
+//       <ul>
+//         {products.map(product => (
+//           <li key={product.id}>{product.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
-export default ProductList;
+// export default ProductList;
