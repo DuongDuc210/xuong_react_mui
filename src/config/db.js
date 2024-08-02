f023688a6632e362f5e039ee21885cb714d3b829
bomongmo <giapntph33546@fpt.edu.vn>
2024-08-02 19:47:19 +0700

    deploy update 3

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,9 @@
-// src/config/db.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose"
 
-export const connectDB = async (uri) => {
+export const conectDB = async (uri) => {
     try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected...');
+        mongoose.connect(uri)
     } catch (error) {
-        console.error('MongoDB connection error:', error);
-        process.exit(1); // Exit process with failure
+        console.log(error);
     }
-};
+}
